106fbbf09c01aa8eef345c2dbcdd951d28132b3a
serenaCho <tkfkd2788@gmail.com>
2021-08-17 21:25:12 +0900

    launcher_page

---
+++ app/src/main/java/com/duksung/and/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.duksung.and;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceStare) {
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
